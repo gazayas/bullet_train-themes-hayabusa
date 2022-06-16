b1bb5459a70722b0d4d9156842a6643f07ee45d7
Gabriel Zayas <g-zayas@hotmail.com>
2022-06-16 22:33:00 +0900

    Put mattr_accessor in right place

--- lib/bullet_train/themes/hayabusa.rb
+++ lib/bullet_train/themes/hayabusa.rb
@@ -5,8 +5,9 @@ require "bullet_train/themes/light"
 module BulletTrain
   module Themes
     module Hayabusa
+      mattr_accessor :color, default: :green
+
       class Theme < BulletTrain::Themes::Light::Theme
-        mattr_accessor :color, default: :green
         def directory_order
           ["hayabusa"] + super
         end
